e2c55acf3cc7063234449ce46d39d2c2088b88ec
flames <flames@192.168.178.20>
2012-03-13 13:23:49 +0100

    fixed pex version check

--- src/main/java/de/luricos/bukkit/WormholeXTreme/Wormhole/plugin/PermissionsSupport.java
+++ src/main/java/de/luricos/bukkit/WormholeXTreme/Wormhole/plugin/PermissionsSupport.java
@@ -43,12 +43,10 @@ public class PermissionsSupport {
      *            the version
      */
     private static void checkPermissionsVersion(String version) {
-    	while(version.length() - version.replaceAll("[^.]", "").length() >= 2) {
-    		version = version.substring(0, version.lastIndexOf("."));
-    	}
-    	Double ver = Double.parseDouble(version);
-        if (ver < 1.8) {
-            WXTLogger.prettyLog(Level.WARNING, false, "Not supported version of PermissionsEx. Recommended is at least 1.8");
+    	version = version.substring(0, version.indexOf(".")) + version.substring(version.indexOf(".")).replace(".", "");
+    	Float ver = Float.parseFloat(version);
+        if (ver < Float.parseFloat("1.18")) {
+            WXTLogger.prettyLog(Level.WARNING, false, "Not supported version of PermissionsEx. Recommended is at least 1.18. Your version is " + ver);
         }
     }
 
